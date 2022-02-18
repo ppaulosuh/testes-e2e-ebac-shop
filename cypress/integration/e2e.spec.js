d6b613e59d3950a915668e1ba4cc2e5bcfac960f
Paulo Victor <p.paulo.suh@gmail.com>
2022-02-18 15:22:54 -0300

    Atualizando para Custom Commands

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -16,34 +16,19 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //Produto 1 
-        cy.get('.product-block').eq(3).click()
-        cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.single_add_to_cart_button').click()
+
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'M', 'Green', '1')
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
-        //Produto 2
-        cy.get('.product-block').eq(4).click()
-        cy.get('.button-variable-item-36').click()
-        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
-        cy.get('.single_add_to_cart_button').click()
+        cy.addProdutos('Abominable Hoodie', 'L', 'Red', '1')
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
-        //Produto 3
-        cy.get('.product-block').eq(0).click()
-        cy.get('.button-variable-item-L').click()
-        cy.get('.button-variable-item-Blue').click()
-        cy.get('.single_add_to_cart_button').click()
+        cy.addProdutos('Aero Daily Fitness Tee', 'XL', 'Brown', '1')
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
-        //Produto 4
-        cy.get('.product-block').eq(8).click()
-        cy.get('.button-variable-item-L').click()
-        cy.get(':nth-child(2) > .value > .variable-items-wrapper').click()
-        cy.get('.single_add_to_cart_button').click()
+        cy.addProdutos('Apollo Running Short', '36', 'Black', '1')
 
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
 
-        EnderecoPage.editarEnderecoFaturamento('Victor','Correa', 'EBAC', 'Brasil', 'Av. Monumental QD 302', 'Bloco H Apto 101', 'Brasilia', 'Distrito Federal', '72800000', '61992001111', 'email@exercicio.com')
+        EnderecoPage.editarEnderecoFaturamento('Victor', 'Correa', 'EBAC', 'Brasil', 'Av. Monumental QD 302', 'Bloco H Apto 101', 'Brasilia', 'Distrito Federal', '72800000', '61992001111', 'email@exercicio.com')
         cy.get('#terms').click()
         cy.get('#place_order').click()
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,10 +24,21 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+//import { tail } from "cypress/types/lodash";
+
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
+    cy.get('[class="product-block grid"]')
+        .contains(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    
+})
